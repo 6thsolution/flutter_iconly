cc24d1e68fab532b1d2e2bd6aa99d7206868c5d5
Payam Zahedi <pzahedi1995@gmail.com>
2021-08-31 22:38:10 +0430

    feat: iconly light pack added

--- test/flutter_iconly_test.dart
+++ test/flutter_iconly_test.dart
@@ -1,6 +1,7 @@
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:flutter_iconly/flutter_iconly.dart';
-
 void main() {
+  test('adds one to input values', () {
+    expect(3, 3);
+  });
 }
